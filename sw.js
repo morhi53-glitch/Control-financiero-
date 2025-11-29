0d9ba8b90e7fcd10de11b42a054e3b5ec3423727
morhi53-glitch <morhi53@gmail.com>
2025-11-29 00:54:21 +0000

    Add service worker with caching functionality
    Implement service worker for caching resources

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
 
+const CACHE_NAME = 'control-marino-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
